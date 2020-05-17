613c099d86c719dcdb3d5e34bb992f709a840635
Pablo Hugen <pablohuggem@gmail.com>
2020-05-17 00:34:27 -0300

    Change theme to dracula
    Also change font to Gohu

--- config.h
+++ config.h
@@ -3,12 +3,14 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[INIT] =   "#282936",     /* after initialization */
+	// [INPUT] =  "#551A8B",   /* during input */
 	[INPUT] =  "#1a3773",   /* during input */
-	//[INPUT] =  "#551A8B",   /* during input */
 	//[FAILED] = "#772b24",   /* wrong password */
-	[FAILED] = "#5e302d",   /* wrong password */
-	[CAPS] = "#382247"      /* CapsLock on */     
+ 	// [FAILED] = "#5e302d",   /* wrong password */
+	[FAILED] = "#282936",   /* wrong password */
+	// [CAPS] = "#382247"      /* CapsLock on */     
+	[CAPS] = "#66d9ef"      /* CapsLock on */     
 };
 
 /* treat a cleared input like a wrong password (color) */
