a2c60d6f70b60111f2bdba016ea4e660f75850cf
Pablo Hugen <pablohuggem@gmail.com>
2021-02-05 11:30:30 -0300

    Change theme to Dracula

--- config.h
+++ config.h
@@ -3,15 +3,15 @@ static const char *user  = "pablo951_br";
 static const char *group = "pablo951_br";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#2e3440",     /* after initialization */
+	[INIT] =   "#1e1f26",     /* after initialization */
 	// [INPUT] =  "#551A8B",   /* during input */
-	[INPUT] =  "#2e3440",   /* during input */
+	[INPUT] =  "#1e1f26",   /* during input */
 	//[FAILED] = "#772b24",   /* wrong password */
  	// [FAILED] = "#5e302d",   /* wrong password */
-	[FAILED] = "#bf616a",   /* wrong password */
+	[FAILED] = "#FF5555",   /* wrong password */
 	// [CAPS] = "#382247"      /* CapsLock on */     
-	[CAPS] = "#3b4252",      /* CapsLock on */     
-	[PAM] =    "#2e3440",   /* waiting for PAM */
+	[CAPS] = "#bd93f94",      /* CapsLock on */     
+	[PAM] =    "#1e1f26",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
