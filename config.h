dcb380c9c9b327f1c8d4e7c4135655c0abccabc1
Pablo <pablohuggem@gmail.com>
2022-06-06 01:47:15 -0300

    Change colorscheme to base16-pico

--- config.h
+++ config.h
@@ -3,15 +3,11 @@ static const char *user  = "pablo951_br";
 static const char *group = "pablo951_br";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#1e1f26",     /* after initialization */
-	// [INPUT] =  "#551A8B",   /* during input */
-	[INPUT] =  "#1e1f26",   /* during input */
-	//[FAILED] = "#772b24",   /* wrong password */
- 	// [FAILED] = "#5e302d",   /* wrong password */
-	[FAILED] = "#FF5555",   /* wrong password */
-	// [CAPS] = "#382247"      /* CapsLock on */     
-	[CAPS] = "#bd93f94",      /* CapsLock on */     
-	[PAM] =    "#1e1f26",   /* waiting for PAM */
+	[INIT] =   "#000000",     /* after initialization */
+	[INPUT] =  "#83769c",   /* during input */
+	[FAILED] = "#ff004d",   /* wrong password */
+	[CAPS] = "#fff024",      /* CapsLock on */     
+	[PAM] =    "#29adff",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
